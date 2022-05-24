fc90f41b6d55b1bf8c04b95cd8a7413094b4d22a
Studio Kaori <studiokaori.com@gmail.com>
2022-05-24 11:51:48 +0200

    Initial Commit

---
+++ NewObjectRecognition/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewObjectRecognition
+//
+//  Created by Kaori Persson on 2022-05-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewObjectRecognition/NewObjectRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewObjectRecognitionApp.swift
+//  NewObjectRecognition
+//
+//  Created by Kaori Persson on 2022-05-24.
+//
+
+import SwiftUI
+
+@main
+struct NewObjectRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
