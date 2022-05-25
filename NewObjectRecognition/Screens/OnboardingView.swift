0be12d24ded5f25c83b42ab2b17378c6af9f394c
Studio Kaori <studiokaori.com@gmail.com>
2022-05-25 11:11:41 +0200

    Add: onboardingview

---
+++ NewObjectRecognition/Screens/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  NewObjectRecognition
+//
+//  Created by Kaori Persson on 2022-05-25.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
