5967505cbf4de5e6959ec8498bb543368a79e9c0
Studio Kaori <studiokaori.com@gmail.com>
2022-05-26 07:18:13 +0200

    Add: State and appstorate

--- NewObjectRecognition/Screens/OnboardingView.swift
+++ NewObjectRecognition/Screens/OnboardingView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
+    // MARK: property
+    // if the isOnboardingViewActive was found in app strage, this would be skipped.
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    // slide button width will be always the screen width - 80(40 padding for each)
+    @State private var slideButtonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var slideButtonOffset: CGFloat = 0
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
