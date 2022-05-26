073d1e3e5babaf9ac357cdf1c234bae79a8c0f50
Studio Kaori <studiokaori.com@gmail.com>
2022-05-26 07:37:56 +0200

    Add: restart button to home screen

--- NewObjectRecognition/Screens/HomeView.swift
+++ NewObjectRecognition/Screens/HomeView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            isOnboardingViewActive = true
+        }) {
+            
+            // the image and the text are in the button label, then these are automatically horizontally aligned.
+            // so don't need to wrap them in hstack.
+            Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                .imageScale(.large)
+            
+            Text("Restart")
+                .font(.system(.title3, design: .rounded))
+                .fontWeight(.bold)
+        } //: Button
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
     }
 }
 
