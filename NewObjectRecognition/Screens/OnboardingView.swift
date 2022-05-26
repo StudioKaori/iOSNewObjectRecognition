3279caf9220e143adbb3f292b4bfba401c56285b
Studio Kaori <studiokaori.com@gmail.com>
2022-05-26 07:31:09 +0200

    Add: Footer to onboarding screen

--- NewObjectRecognition/Screens/OnboardingView.swift
+++ NewObjectRecognition/Screens/OnboardingView.swift
@@ -61,8 +61,85 @@ struct OnboardingView: View {
                 Spacer()
                 
                 // MARK: - footer
-            }
-        }
+                
+                ZStack {
+                    // 1. bg static
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. call to action static
+                    
+                    Text("Get started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .offset(x: 20)
+                    
+                    // 3. capsule (dynamic width)
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorPink"))
+                            // buttonOffset will start from 0, so add 80
+                            .frame(width: slideButtonOffset + circleButtonWidth)
+                        
+                        // To push the red circle to the left
+                        Spacer()
+                    }
+                    // 4. circle (draggable)
+                    
+                    HStack {
+                        ZStack{
+                            Circle()
+                                .fill(Color("ColorPink"))
+                            
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: circleButtonWidth, height: circleButtonWidth, alignment: .center)
+                        .offset(x: slideButtonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged{ gesture in
+                                    // This is triggered each time the drag gesture happened.
+                                    
+                                    // dragging direction left to right
+                                    // gesture.translation is equivalent to location.{x,y} - startLocation.{x,y}. = amount of dragging
+                                    if gesture.translation.width > 0 && slideButtonOffset <= slideButtonWidth - circleButtonWidth {
+                                        slideButtonOffset = gesture.translation.width
+                                    }
+                                }
+                                .onEnded { _ in
+                                    if slideButtonOffset > slideButtonWidth / 2 {
+                                        // when the button is in the right than half, move to the home screen
+                                        slideButtonOffset = slideButtonWidth - circleButtonWidth
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        // the button is in the left than the half, back to the default position
+                                        slideButtonOffset = 0
+                                    }
+                                    
+                                }
+                        ) //: Gesture
+                        
+                        // To push the button to the left edge
+                        Spacer()
+                    } //: ZStack
+                    
+                    
+                } //: footer
+                .frame(width: slideButtonWidth, height: 80, alignment: .center)
+                .padding()
+            } //: VSTACK
+        } //: ZSTACK
     }
 }
 
