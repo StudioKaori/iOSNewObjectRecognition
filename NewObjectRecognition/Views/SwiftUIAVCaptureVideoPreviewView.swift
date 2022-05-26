eeb82af39b81541aa663328fab40dc13494e2e08
Studio Kaori <studiokaori.com@gmail.com>
2022-05-26 07:45:29 +0200

    Add: SwiftUIAVCaptureVideoPreviewView

---
+++ NewObjectRecognition/Views/SwiftUIAVCaptureVideoPreviewView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIAVCaptureVideoPreviewView.swift
+//  NewObjectRecognition
+//
+//  Created by Kaori Persson on 2022-05-26.
+//
+
+import SwiftUI
+
+struct SwiftUIAVCaptureVideoPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIAVCaptureVideoPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIAVCaptureVideoPreviewView()
+    }
+}
